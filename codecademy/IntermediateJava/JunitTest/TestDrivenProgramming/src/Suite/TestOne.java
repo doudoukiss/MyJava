0291b99a163229faf30ac73800fae686b282837d
Seattle <Rayallen1996@gmail.com>
2023-04-08 05:36:48 +0800

    no message

---
+++ codecademy/IntermediateJava/JunitTest/TestDrivenProgramming/src/Suite/TestOne.java
@@ -0,0 +1,14 @@
+package Suite;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestExample {
+  String test = "Test one!"
+
+  @Test
+  public void test2() {
+    assertThat(test, equalTo(“Test one!”));
+}
