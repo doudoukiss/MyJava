d84cb5d222b06ee298abb3bca443d306c22a9e6f
doudoukiss <rayallen1996@gmail.com>
2023-04-15 22:29:11 +0900

    0415

---
+++ IOS/YoungStarsApp/YoungStarsApp/YoungStarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoungStarsAppApp.swift
+//  YoungStarsApp
+//
+//  Created by Seattle on 2023/4/15.
+//
+
+import SwiftUI
+
+@main
+struct YoungStarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
