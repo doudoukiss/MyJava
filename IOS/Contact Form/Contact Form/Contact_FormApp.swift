2cdf38458644dbd6e9f1476ef0edcd7291349c95
doudoukiss <rayallen1996@gmail.com>
2023-06-18 10:53:19 +0900

    0618

---
+++ IOS/Contact Form/Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Seattle on 2023/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS/Contact Form/Contact Form/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Contact Form
+//
+//  Created by Seattle on 2023/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+                Text("胡连馨鹅")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
