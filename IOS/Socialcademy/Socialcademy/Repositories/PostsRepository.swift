4c770f42b0bc76eb9d9da5b4a56ad1ce6b91bda9
doudoukiss <rayallen1996@gmail.com>
2023-07-07 12:17:12 +0900

    0707

--- IOS/Socialcademy/Socialcademy/Repositories/PostsRepository.swift
+++ IOS/Socialcademy/Socialcademy/Repositories/PostsRepository.swift
@@ -14,6 +14,7 @@ import FirebaseFirestoreSwift
 protocol PostsRepositoryProtocol {
     func fetchPosts() async throws -> [Post]
     func create(_ post: Post) async throws
+    func delete(_ post: Post) async throws
 }
 
 // MARK: - PostsRepositoryStub
@@ -27,6 +28,8 @@ struct PostsRepositoryStub: PostsRepositoryProtocol {
     }
     
     func create(_ post: Post) async throws {}
+    
+    func delete(_ post: Post) async throws {}
 }
 #endif
 
@@ -48,13 +51,16 @@ struct PostsRepository: PostsRepositoryProtocol {
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
+    
+    func delete(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.delete()
+    }
 }
 
 private extension DocumentReference {
     func setData<T: Encodable>(from value: T) async throws {
         return try await withCheckedThrowingContinuation { continuation in
-            // Method only throws if there’s an encoding error, which indicates a problem with our model.
-            // We handled this with a force try, while all other errors are passed to the completion handler.
             try! setData(from: value) { error in
                 if let error = error {
                     continuation.resume(throwing: error)
---
+++ IOS/Socialcademy/Socialcademy/Supporting Views/Alert+Error.swift
@@ -0,0 +1,43 @@
+//
+//  Alert+Error.swift
+//  Socialcademy
+//
+//  Created by Seattle on 2023/7/7.
+//
+
+import SwiftUI
+
+extension View {
+    func alert(_ title: String, error: Binding<Error?>) -> some View {
+        modifier(ErrorAlertViewModifier(title: title, error: error))
+    }
+}
+
+private struct ErrorAlertViewModifier: ViewModifier {
+    let title: String
+    @Binding var error: Error?
+    
+    func body(content: Content) -> some View {
+        content
+            .alert(title, isPresented: $error.hasValue, presenting: error, actions: { _ in }) { error in
+                Text(error.localizedDescription)
+            }
+    }
+}
+
+private extension Optional {
+    var hasValue: Bool {
+        get { self != nil }
+        set { self = newValue ? self : nil }
+    }
+}
+
+struct ErrorAlertViewModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Preview")
+            .alert("Error", error: .constant(PreviewError()))
+    }
+    private struct PreviewError: LocalizedError {
+        let errorDescription: String? = " Seattle Supersonics"
+    }
+}
