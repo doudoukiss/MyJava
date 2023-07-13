8752fcf660d303022d24d705fa7085349094dcfb
doudoukiss <rayallen1996@gmail.com>
2023-07-13 10:22:43 +0900

    0713

---
+++ IOS/Boxueio/variables-and-contants.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var swiftString = " Seattle "
+
+var hours =24
+
+var PI = 3.14
+
+var swiftIsFun = true
+
+var me = ("Seattle",34, "Rayallen@gmail.com") // type inference
+me.0
+me.1
+me.2
+
+PI = 3.1415
+ let minutes = 30
+ let fireIsHot = true
+// fireIsHot = false
+
+var x: Int
+var s : String //type annotation
+
