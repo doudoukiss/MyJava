d7e1e81ff7fb25647237c292056c38b6d9557625
doudoukiss <rayallen1996@gmail.com>
2023-07-20 00:04:40 +0800

    0720a

--- IOS/Boxueio/2Swift3Collection.swift
+++ IOS/Boxueio/2Swift3Collection.swift
@@ -146,3 +146,174 @@ animals.myFlatMap { animal in
     return ids.map { id in (animal, id) }
 }
 
+enum RecordType {
+    case bool(Bool)
+    case number(Int)
+    case text(String)
+}
+
+enum RecordType {
+    case bool(Bool)
+    case number(Int)
+    case text(String)
+}
+
+let record11: [String: RecordType] = [
+    "uid": .number(11),
+    "exp": .number(100),
+    "favourite": .bool(true),
+    "title": .text("Dictionary basics")
+]
+
+for key in record11.keys { print(key) }
+record11.keys.forEach { print($0) }
+
+for (k, v) in record10 {
+    print("\(k): \(v)")
+}
+
+record10.forEach { print("\($0): \($1)") }
+
+for key in record10.keys.sorted() {
+    print("\(key): \(record10[key])")
+}
+
+let defaultRecord: [String: RecordType] = [
+    "uid": .number(0),
+    "exp": .number(100),
+    "favourite": .bool(false),
+    "title": .text("")
+]
+
+var template = defaultRecord
+var record11Patch: [String: RecordType] = [
+    "uid": .number(11),
+    "title": .text("Common dictionary extensions")
+]
+
+extension Dictionary {
+    mutating func merge<S:Sequence>(_ sequence: S)
+        where S.Iterator.Element == (key: Key, value: Value) {
+
+        sequence.forEach { self[$0] = $1 }
+    }
+
+    init<S:Sequence>(_ sequence: S)
+        where S.Iterator.Element == (key: Key, value: Value) {
+
+        self = [:]
+        self.merge(sequence)
+    }
+
+    func mapValue<T>(_ transform: (Value) -> T) -> [Key: T] {
+        return Dictionary<Key, T>(map { (k, v) in
+            return (k, transform(v))
+        })
+    }
+
+}
+let record10Patch: [(key: String, value: RecordType)] = [
+    (key: "uid",   value: .number(10)),
+    (key: "title", value: .text("Common dictionary extensions"))
+]
+
+var template1 = defaultRecord
+template1.merge(record10Patch)
+let record11 = Dictionary(record11Patch)
+record11.map { $1 }
+
+let newRecord11 = record11.mapValue { record -> String in
+    switch record {
+    case .text(let title):
+        return title
+    case .number(let exp):
+        return String(exp)
+    case .bool(let favourite):
+        return String(favourite)
+    }
+}
+
+ struct Account {
+    var alias: String
+    var type: Int
+    var createdAt: Date
+
+    let INT_BIT = (Int)(CHAR_BIT) * MemoryLayout<Int>.size
+
+    func bitwiseRotate(value: Int, bits: Int) -> Int {
+        return (((value) << bits) | ((value) >> (UINT_BIT - bits)))
+    }
+
+}
+
+extension Account: Hashable {
+    var hashValue: Int
+}
+extension Account: Hashable {
+    var hashValue: Int {
+        return alias.hashValue ^
+                type.hashValue ^
+                createdAt.hashValue
+    }
+}
+extension Account: Equatable {
+    static func == (lhs: Account, rhs: Account) -> Bool {
+        return lhs.alias == rhs.alias &&
+            lhs.type == rhs.type &&
+            lhs.createdAt == rhs.createdAt
+    }
+}
+
+extension Account: Hashable {
+    var hashValue: Int {
+        return bitwiseRotate(value: alias.hashValue, bits: 10) ^
+            type.hashValue ^
+            createdAt.hashValue
+    }
+}
+
+var vowel: Set<Character> = ["a", "e", "i", "o", "u"]
+vowel.count .isEmpty  contains  remove insert removeAll
+for character in vowel {
+    print(character)
+}
+vowel.forEach { print($0) }
+for character in vowel.sorted() {
+    print(character)
+}
+
+var setA: Set = [1, 2, 3, 4, 5, 6]
+var setB: Set = [4, 5, 6, 7, 8, 9]
+let interSectAB: Set = setA.intersection(setB)
+let symmetricDiffAB: Set = setA.symmetricDifference(setB)
+let unionAB: Set = setA.union(setB)
+let aSubstractB: Set = setA.subtracting(setB)
+setA.formIntersection(setB)
+
+extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var result: Set<Iterator.Element> = []
+        
+        return filter {
+            if result.contains($0) {
+                return false
+            }
+            else {
+                result.insert($0)
+                return true
+            }
+        }
+    }
+}
+let oneToSix: IndexSet = [1, 2, 3, 4, 5, 6]
+let hw = "Hello world!"
+
+let numbers = CharacterSet(charactersIn: "123456789")
+let letters = CharacterSet.letters
+
+hw.rangeOfCharacter(from: numbers) 
+hw.rangeOfCharacter(from: letters)
+
+for i in stride(from: 1.0, to: 5.0, by: 1.0) {
+    print(i)
+}
