1382d1e09052797a9779b66734094d4b6a7206fb
doudoukiss <rayallen1996@gmail.com>
2023-09-06 20:51:21 +0900

    0906

---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Seattle on 2023/9/6.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
