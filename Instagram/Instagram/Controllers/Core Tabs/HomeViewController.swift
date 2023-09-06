ed69445d0b548a138b9fec72893d40de794c3e27
doudoukiss <rayallen1996@gmail.com>
2023-09-06 22:44:52 +0900

    0906b

--- Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Seattle on 2023/9/6.
 //
 
+import FirebaseAuth
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -12,6 +13,21 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handleNotAuthenticated()
+    }
+    
+    private func handleNotAuthenticated() {
+        // Check auth status
+        if Auth.auth().currentUser == nil {
+            // Show Log In
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: false)
+        }
     }
 
 
--- Instagram/Instagram/Resources/AppDelegate.swift
+++ Instagram/Instagram/Resources/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Seattle on 2023/9/6.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
