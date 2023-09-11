5a5e7ac2704ac29c9f7eaf4f610f397bdac3b101
doudoukiss <rayallen1996@gmail.com>
2023-09-11 16:22:46 +0900

    0911Part11

--- Instagram/Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -130,14 +130,24 @@ extension ProfileViewController: ProfileInfoHeaderCollectionReusableViewDelegate
     
     func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView) {
         // show the list controller to show the followers
-        let vc = ListViewController(data: ["向涵之", "周也","胡连馨","赵今麦"])
+        var mockData = [UserRelationship]()
+        for x in 0..<10 {
+            mockData.append(UserRelationship(username: "@向涵之", name: "向涵之", type: x % 2 == 0 ? .following : .not_following))
+        }
+        //let vc = ListViewController(data: ["向涵之", "周也","胡连馨","赵今麦"])
+        let vc = ListViewController(data: mockData)
         vc.title = "Followers"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
     
     func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView) {
-        let vc = ListViewController(data: ["向涵之", "周也","胡连馨","赵今麦"])
+        var mockData = [UserRelationship]()
+        for x in 0..<10 {
+            mockData.append(UserRelationship(username: "@向涵之", name: "向涵之", type: x % 2 == 0 ? .following : .not_following))
+        }
+        //let vc = ListViewController(data: ["向涵之", "周也","胡连馨","赵今麦"])
+        let vc = ListViewController(data: mockData)
         vc.title = "Following"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
--- Instagram/Instagram/Views/Cells/UserFollowTableViewCell.swift
+++ Instagram/Instagram/Views/Cells/UserFollowTableViewCell.swift
@@ -8,17 +8,30 @@
 import UIKit
 
 protocol UserFollowTableViewCellDelegate: AnyObject {
-    func didTapFollowUnfollowButton(model: String)
+    func didTapFollowUnfollowButton(model: UserRelationship)
 }
 
+enum FollowState {
+    case following, not_following
+}
+
+struct UserRelationship {
+    let username: String
+    let name: String
+    let type: FollowState
+}
 class UserFollowTableViewCell: UITableViewCell {
 
     static let identifier = "UserFollowTableViewCell"
     
     weak var delegate: (UserFollowTableViewCellDelegate)?
     
+    private var model: UserRelationship?
+    
     private let profileImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.backgroundColor = .secondarySystemBackground
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
@@ -27,6 +40,7 @@ class UserFollowTableViewCell: UITableViewCell {
         let label = UILabel()
         label.numberOfLines = 1
         label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.text = "向涵之"
         return label
     }()
     
@@ -34,11 +48,14 @@ class UserFollowTableViewCell: UITableViewCell {
         let label = UILabel()
         label.numberOfLines = 1
         label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.text = "@向涵之"
+        label.textColor = .secondaryLabel
         return label
     }()
     
     private let followButton: UIButton = {
         let button = UIButton()
+        button.backgroundColor = .link
         return button
     }()
     
@@ -49,10 +66,37 @@ class UserFollowTableViewCell: UITableViewCell {
         contentView.addSubview(usernameLabel)
         contentView.addSubview(profileImageView)
         contentView.addSubview(followButton)
+        selectionStyle = .none
+
+        followButton.addTarget(self, action: #selector(didTapFollowersButton), for: .touchUpInside)
     }
     
-    public func configure(with model: String) {
-        
+    @objc private func didTapFollowersButton() {
+        guard let model = model else {
+            return
+        }
+        delegate?.didTapFollowUnfollowButton(model: model)
+    }
+    
+    public func configure(with model: UserRelationship) {
+        self.model = model
+        nameLabel.text = model.name
+        usernameLabel.text = model.username
+        switch model.type {
+        case .following:
+            // show unfollow button
+            followButton.setTitle("Unfollow", for: .normal)
+            followButton.setTitleColor(.label, for: .normal)
+            followButton.backgroundColor = .systemBackground
+            followButton.layer.borderWidth = 1
+            followButton.layer.borderColor = UIColor.label.cgColor
+        case .not_following:
+            // show follow button
+            followButton.setTitle("Follow", for: .normal)
+            followButton.setTitleColor(.label, for: .normal)
+            followButton.backgroundColor = .link
+            followButton.layer.borderWidth = 0
+        }
     }
     
     override func prepareForReuse() {
@@ -67,6 +111,16 @@ class UserFollowTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        profileImageView.frame = CGRect(x: 3, y: 3, width: contentView.height - 6, height: contentView.height - 6)
+        profileImageView.layer.cornerRadius = profileImageView.height/2.0
+        
+        let buttonWidth = contentView.width > 500 ? 220.0 : contentView.width/3
+        followButton.frame = CGRect(x: contentView.width - 5 - buttonWidth, y: (contentView.height - 40)/2, width: buttonWidth, height: 40)
+        
+        let labelHeight = contentView.height/2
+        nameLabel.frame = CGRect(x: profileImageView.right + 5, y: 0, width: contentView.width - 8 - profileImageView.width - buttonWidth, height: labelHeight)
+        usernameLabel.frame = CGRect(x: profileImageView.right + 5, y: nameLabel.bottom, width: contentView.width - 8 - profileImageView.width - buttonWidth, height: labelHeight)
     }
     
     required init?(coder: NSCoder) {
