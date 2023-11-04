46cb09ba7a20d2ba01e21fa724248025a3dd5295
doudoukiss <rayallen1996@gmail.com>
2023-11-04 16:30:01 +0900

    1104a

---
+++ IOSAcademy/Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Seattle on 2023/11/4.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
